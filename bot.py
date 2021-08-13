34495a187f4b8349d6d9daeef12c2f5db143a6ae
kintama48 <65843525+kintama48@users.noreply.github.com>
2021-08-13 08:34:09 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,7 +34,6 @@ def alert_found(text):
     text = text.replace("#alert", "").strip()
     text = text.replace("#Alert", "").strip()
     text = text.replace("#ALERT", "").strip()
-    print("Inside alert_found")
     if "SOLD" in text or "sold" in text or "Sold" in text:
         text = text.replace("sold", "").strip()
         text = text.replace("Sold", "").strip()
@@ -50,19 +49,16 @@ def alert_found(text):
 
 
 async def send_to_alert(embed):
-    print("Inside send_to_alert")
     await client.get_channel(alert_channel_id).send(content="@everyone", embed=embed)
     return
 
 
 async def send_to_all(embed):
-    print("Inside send_to_all")
     await client.get_channel(all_channel_id).send(content="@everyone", embed=embed)
     return
 
 
 async def send_to_one(text):
-    print("Inside send to one")
     await client.get_channel(all_channel_id).send(content=f"@everyone\n{text}")
     return
 
@@ -75,42 +71,39 @@ async def on_ready():
     last_tweet = '0'
 
     while True:
-        current_last_tweet = api.user_timeline(screen_name=USER_TO_SNITCH, count=1, include_rts=False, tweet_mode='extended')[0]
+        current_last_tweet = \
+        api.user_timeline(screen_name=USER_TO_SNITCH, count=1, include_rts=False, tweet_mode='extended')[0]
         if (int(current_last_tweet.id_str) > int(last_tweet)) and (
                 not current_last_tweet.full_text.startswith('RT')):
             last_tweet = current_last_tweet.id_str
             text = current_last_tweet.full_text
-            if "#chart" not in text or "#CHART" not in text or "#Chart" not in text:
+            print(text)
+            if "#chart" not in text and "#CHART" not in text and "#Chart" not in text:
                 if "#alert" in text or "#Alert" in text or "#ALERT" in text:
-                    print("Inside if on_ready")
                     embed = alert_found(text)
                     # await send_to_alert(embed)
                     # await send_to_all(embed)
                     await asyncio.gather(send_to_alert(embed), send_to_all(embed))
                 else:
-                    print("Inside else on_ready")
                     await asyncio.gather(send_to_one(current_last_tweet.full_text))
             else:
                 media_link = current_last_tweet.extended_entities["media"][0]["media_url_https"]
                 text = current_last_tweet.full_text.split()
                 text.pop()
                 text = " ".join(text)
-                if "#Charts" in text:
-                    text = text.replace("#Charts", "").strip()
-                elif "#CHARTS" in text:
-                    text = text.replace("#CHARTS", "").strip()
-                elif "#charts" in text:
-                    text = text.replace("#charts", "").strip()
-                elif "#chart" in text:
-                    text = text.replace("#chart", "").strip()
-                elif "#Chart" in text:
-                    text = text.replace("#Chart", "").strip()
-                elif "#CHART" in text:
-                    text = text.replace("#CHART", "").strip()
-
+                text = text.replace("#Charts", "").strip()
+                text = text.replace("#CHARTS", "").strip()
+                text = text.replace("#charts", "").strip()
+                text = text.replace("#chart", "").strip()
+                text = text.replace("#Chart", "").strip()
+                text = text.replace("#CHART", "").strip()
+                if text == "":
+                    text = " "
                 media_embed = discord.Embed(color=0xffd500, description=f"@everyone\n**{text}**").set_image(
                     url=media_link)
                 await asyncio.gather(client.get_channel(charts_channel_id).send(embed=media_embed))
+
         time.sleep(10)
+
 
 client.run(DISCORD_BOT_TOKEN)
